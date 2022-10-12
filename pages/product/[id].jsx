ccc037bb3f5845c8eb3ca3770da82fded1823283
anwari-fikri <anwari.fikri@gmail.com>
2022-10-12 22:34:57 +0800

    Styled product page

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -17,64 +17,81 @@ const Product = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <Image src={pizza.img} alt="" width={500} height={500} />
-        </div>
-      </div>
-      <div>
-        <div>
-          <h1>{pizza.name}</h1>
-          <span>${pizza.price[size]}</span>
-          <p>{pizza.desc}</p>
-        </div>
-        <div>
-          <label>
-            <h3>Choose the size</h3>
-            <select value={size} onChange={changeSize}>
-              <option value={0}>Small</option>
-              <option value={1}>Medium</option>
-              <option value={2}>Large</option>
-            </select>
-          </label>
-        </div>
-        {/* <div>
-          <h3>Choose Size</h3>
-          <div onClick={() => setSize(0)}>
-            <p className="active:underline">Small</p>
+    <div className="pt-28 w-full">
+      <div className="max-w-[85%] h-full m-auto py-28 xl:max-w-[1240px] md:py-40">
+        <div className="grid lg:grid-cols-2 gap-8">
+          <div className="flex justify-center items-center">
+            <div>
+              <Image src={pizza.img} alt="" width={500} height={500} />
+            </div>
           </div>
-          <div onClick={() => setSize(1)}>
-            <p>Medium</p>
-          </div>
-          <div onClick={() => setSize(2)}>
-            <p>Large</p>
-          </div>
-        </div> */}
-        <div>
-          <h3>Choose additional ingredient</h3>
           <div>
-            <div>
-              <input type="checkbox" id="double" name="double" />
-              <label htmlFor="double">Double Ingredients</label>
+            <div className="mb-5">
+              <div className="pb-2">
+                <h1>{pizza.name}</h1>
+                <p>{pizza.desc}</p>
+              </div>
+              <div>
+                <h2>${pizza.price[size]}</h2>
+              </div>
             </div>
-            <div>
-              <input type="checkbox" id="cheese" name="cheese" />
-              <label htmlFor="cheese">Extra Cheese</label>
+            <div className="p-5 bg-white rounded-3xl mb-3">
+              <label>
+                <h3 className="pb-1">Choose the size</h3>
+                <select
+                  value={size}
+                  onChange={changeSize}
+                  className="outline outline-1 rounded-md py-2 px-4 bg-gray-100"
+                >
+                  <option value={0}>Small</option>
+                  <option value={1}>Medium</option>
+                  <option value={2}>Large</option>
+                </select>
+              </label>
             </div>
-            <div>
-              <input type="checkbox" id="spicy" name="spicy" />
-              <label htmlFor="spicy">Spicy Sauce</label>
+            <div className="p-5 bg-white rounded-3xl mb-3">
+              <h3 className="pb-1">Choose additional ingredient</h3>
+              <div>
+                <div>
+                  <input type="checkbox" id="double" name="double" />
+                  <label htmlFor="double" className="ml-2">
+                    Double Ingredients
+                  </label>
+                </div>
+                <div>
+                  <input type="checkbox" id="cheese" name="cheese" />
+                  <label htmlFor="cheese" className="ml-2">
+                    Extra Cheese
+                  </label>
+                </div>
+                <div>
+                  <input type="checkbox" id="spicy" name="spicy" />
+                  <label htmlFor="spicy" className="ml-2">
+                    Spicy Sauce
+                  </label>
+                </div>
+                <div>
+                  <input type="checkbox" id="garlic" name="garlic" />
+                  <label htmlFor="garlic" className="ml-2">
+                    Garlic Sauce
+                  </label>
+                </div>
+              </div>
             </div>
-            <div>
-              <input type="checkbox" id="garlic" name="garlic" />
-              <label htmlFor="garlic">Garlic Sauce</label>
+            <div className="p-5 bg-white rounded-3xl mb-5">
+              <h3 className="pb-1">Choose amount</h3>
+              <input
+                type="number"
+                defaultValue={1}
+                className="outline outline-1 rounded-md p-2 bg-gray-100"
+              />
+            </div>
+            <div className="flex justify-center pb-3">
+              <button className="bg-[#D1411E] py-3 px-6 text-white rounded-md">
+                Add to cart
+              </button>
             </div>
           </div>
-        </div>
-        <div>
-          <input type="number" defaultValue={1} />
-          <button>Add to card</button>
         </div>
       </div>
     </div>
