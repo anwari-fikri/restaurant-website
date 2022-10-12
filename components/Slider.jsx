d57d30318647a847c9c3806db66ff8b9113666ce
anwari-fikri <anwari.fikri@gmail.com>
2022-10-12 13:29:33 +0800

    Added a better carousel using Swiper js

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,88 +1,53 @@
-import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
-import { AiOutlineVerticalLeft, AiOutlineVerticalRight } from "react-icons/ai";
+import React, { useEffect, useRef } from "react";
 
-const featuredProducts = [
-  "/assets/ww.png",
-  "/assets/ww2.png",
-  "/assets/ww3.png",
-];
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
-let count = 0;
-let slideInterval;
-const Slider = ({ nav }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+const Slider2 = ({ nav }) => {
   const slideRef = useRef();
-
-  const removeAnimation = () => {
-    slideRef.current.classList.remove("fade-anim");
-  };
-
-  useEffect(() => {
-    slideRef.current.addEventListener("animationend", removeAnimation);
-    startSlider();
-    return () => {
-      pauseSlider();
-    };
-  }, []);
 
   useEffect(() => {
     console.log(nav);
     nav
-      ? slideRef.current.classList.add("-z-10")
-      : slideRef.current.classList.remove("-z-10");
+      ? (slideRef.current.style.zIndex = -1)
+      : (slideRef.current.style.zIndex = 0);
   }, [nav]);
 
-  const startSlider = () => {
-    slideInterval = setInterval(() => {
-      handleOnNextClick();
-    }, 3000);
-  };
-
-  const pauseSlider = () => {
-    clearInterval(slideInterval);
-  };
-
-  const handleOnNextClick = () => {
-    count = (count + 1) % featuredProducts.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
-  const handleOnPreviousClick = () => {
-    count =
-      (currentIndex + featuredProducts.length - 1) % featuredProducts.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
-
   return (
-    <div ref={slideRef} className="w-full select-none relative">
-      <div className="">
-        <Image
-          src={featuredProducts[currentIndex]}
-          alt=""
-          width={1920}
-          height={900}
-        />
-      </div>
-
-      <div className="absolute w-full top-1/2 px-3 flex justify-between items-center">
-        <button
-          onClick={handleOnPreviousClick}
-          className="bg-black text-white p-1 z-50 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-        >
-          <AiOutlineVerticalRight size={30} />
-        </button>
-        <button
-          onClick={handleOnNextClick}
-          className="bg-black text-white p-1 z-50 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-        >
-          <AiOutlineVerticalLeft size={30} />
-        </button>
-      </div>
-    </div>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      style={{
+        "--swiper-theme-color": "#D1411E",
+        zIndex: 0,
+      }}
+      ref={slideRef}
+    >
+      <SwiperSlide>
+        <img src="assets/ww.png" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="assets/ww2.png" alt="" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="assets/ww3.png" alt="" />
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
-export default Slider;
+export default Slider2;
